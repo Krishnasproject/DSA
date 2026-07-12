74f85aaf206226b6f77b8bc3caff8d4098c89ef1
Krishnanand Jha <142677423+Krishnasproject@users.noreply.github.com>
2026-07-12 16:52:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (10.27%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    bool func(vector<int>&nums,int n ,int limit,int k){
+        int k1 = 1;
+        int sum = 0;
+
+        for(int i = 0;i<n;i++){
+            if(sum + nums[i] <=limit)
+            {
+                sum += nums[i];
+            }
+            else
+                {
+                    k1++;
+                    sum = nums[i];
+
+                    if(k1>k)
+                        return false;
+                }
+        }
+        return true;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+            if(n<k)
+                return -1;
+    
+        int low=0,high = 0;
+        for(int i = 0;i<n;i++){
+            low = max(low,nums[i]);
+            high+=nums[i];
+        }
+        int res = -1;
+        while(low<=high){
+            int guess = low + (high-low)/2;
+
+            if(func(nums,n,guess,k)){
+                res = guess;
+                high = guess-1;
+            }
+            else{
+                low = guess+1;
+            }
+
+        }
+        return res;
+    }
+};
