aa3ce5922a7571baedb993b56a27ecc538224e62
Krishnanand Jha <142677423+Krishnasproject@users.noreply.github.com>
2026-07-16 23:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (73.03%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
 
-    bool func(vector<int>&nums,int n ,int limit,int k){
+    bool func(vector<int>&nums, int n , int guess,int k ){
         int k1 = 1;
         int sum = 0;
 
         for(int i = 0;i<n;i++){
-            if(sum + nums[i] <=limit)
+            if(sum+nums[i]<=guess)
             {
                 sum += nums[i];
             }
             else
-                {
-                    k1++;
-                    sum = nums[i];
+            {
+                k1++;
+                sum = nums[i];
 
-                    if(k1>k)
-                        return false;
-                }
+                if(k1>k)
+                    return false;
+            }
+            
         }
         return true;
     }
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
-            if(n<k)
-                return -1;
-    
-        int low=0,high = 0;
-        for(int i = 0;i<n;i++){
-            low = max(low,nums[i]);
-            high+=nums[i];
-        }
-        int res = -1;
-        while(low<=high){
-            int guess = low + (high-low)/2;
 
-            if(func(nums,n,guess,k)){
+        if(n<k)
+            return -1;
+
+        int low = 0,high = 0 ;
+        int res = -1;
+        for(int i =0;i<n;i++){
+            low = max(low,nums[i]);
+            high += nums[i];
+        }
+
+        while(low<=high){
+            int guess = low + (high - low)/2;
+
+            if(func(nums,n,guess, k ))
+            {
                 res = guess;
-                high = guess-1;
+                high = guess - 1;
             }
             else{
-                low = guess+1;
+                low = guess + 1;
             }
-
         }
         return res;
     }
