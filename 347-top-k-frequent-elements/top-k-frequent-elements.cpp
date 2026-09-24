02f304bd21dfd4e898137f3cfc38c852c071e168
Krishnanand Jha <142677423+Krishnasproject@users.noreply.github.com>
2026-09-24 21:28:58 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (77.09%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
+    
+    struct cmp{
+        bool operator()(pair<int,int>&a,pair<int,int>&b){
+            if(a.first != b.first)
+                return a.first>b.first;//min heap behaviour 
+
+            return a.second>b.second;
+        }
+    };
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> ans;
-        unordered_map<int, int> mp;
+        unordered_map<int,int>mp;
 
-        for (int i = 0; i < n; i++) {
-            mp[nums[i]]++;
+        for(int num : nums)
+            mp[num]++;
+
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
+
+        for(auto i : mp){
+            pq.push({i.second,i.first});
+            
+            if(pq.size()>k)
+                pq.pop();
         }
 
-        priority_queue<pair<int, int>> pq;
+        vector<int>ans;
 
-        for (auto it : mp) {
-            pq.push({it.second, it.first});
-        }
-
-        while (k > 0) {
+        while(!pq.empty()){
             ans.push_back(pq.top().second);
             pq.pop();
-            k--;
         }
-
         return ans;
     }
 };
